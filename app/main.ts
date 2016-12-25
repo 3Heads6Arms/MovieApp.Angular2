244ddd74bf1b3622b85fd3663c00a393a4e44a7c
Anh Hoang <Anh Hoang>
2016-12-25 16:10:10 +0200

    added css bundling to webpack

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,7 @@
 import './polyfills';
-import './vendors'
+import './vendors';
+import './app.theme.css';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { MovieAppModule } from './app.module';
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,17 @@
 "use strict";
 
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: {
         main: './app/main.ts'
     },
     output: {
-        filename: './app/bundle.js',
+        filename: './app/[name].bundle.js',
     },
     resolve: {
-        extensions: ['', '.js', '.ts', '.html', '.css']
+        extensions: ['', '.js', '.ts', '.html', '.css', '.scss']
     },
     devServer: {
         contentBase: ".",
@@ -22,7 +23,12 @@ module.exports = {
         loaders: [
             { test: /\.ts?$/, loader: 'ts-loader' },
             { test: /\.html?$/, loader: 'html-loader' },
-            { test: /\.css?$/, loader: 'css-loader' }
+            { test: /\.css?$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
+            { test: /\.scss?$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader') },
+            { test: /\.less?$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader') }
         ],
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin("./app/app.css", { allChunks: false })
+    ]
 };
