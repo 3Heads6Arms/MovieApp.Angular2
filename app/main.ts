6440d8d720883c5e975b24a623e0ba65c1ac2f81
Anh Hoang <Anh Hoang>
2016-12-25 17:59:05 +0200

    angular material init

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,10 @@
 import './polyfills';
 import './vendors';
+import 'hammerjs';
 import './app.theme.css';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { MovieAppModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(MovieAppModule);
+platformBrowserDynamic().bootstrapModule(MovieAppModule); 
